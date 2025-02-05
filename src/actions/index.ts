992fad2a8fb2a4bc3831f6369d212113ee82817a
mark on github <markongithub@users.noreply.github.com>
2025-02-04 20:57:46 -0500

    add the like action but I don't feel good about it

---
+++ src/actions/index.ts
@@ -0,0 +1,38 @@
+// should z come from astro:schema?
+// import { z } from 'astro:schema';
+import { defineAction, z } from "astro:actions";
+import { db, Likes, gt, sql } from "astro:db";
+
+export const server = {
+    like: defineAction({
+        input: z.object({
+            postId: z.string(),
+            liked: z.boolean()
+        }),
+        handler: async ({ postId, liked }) => {
+            const mutate = await db
+                .insert(Likes)
+                .values({
+                    postId: postId,
+                    likes: liked ? 1 : 0,
+                })
+                .onConflictDoUpdate(
+                    liked
+                        ? {
+                            target: Likes.postId,
+                            set: { likes: sql`likes + 1` }
+                        }
+                        : {
+                            target: Likes.postId,
+                            set: { likes: sql`likes -1` },
+                            where: gt(Likes.likes, 0)
+                        }
+                )
+                .returning()
+                .get();
+
+            return mutate?.likes ?? 0
+
+        }
+    }),
+};
